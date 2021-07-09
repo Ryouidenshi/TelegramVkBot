e55467432b2f7d385ec08c6fc5db7f69c729259a
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2021-07-10 00:55:20 +0500

    Update enums.py

--- enums.py
+++ enums.py
@@ -3,23 +3,45 @@ from enum import Enum
 
 class ButtonsType(Enum):
     AdminPanel = 'admin'
+    ModeratorPanel = 'moderator'
     FunctionalPanel = 'select_func'
     SelectFirstFunction = 'first_func'
     SelectSecondFunction = 'second_func'
     RemoveButtons = 'remove'
+    YesOrNo = 'yesOrNo'
+    ActualButtons = 'actual'
     Stop = 'Остановить'
     ShowResult = 'Показать результат'
     FirstFunction = 'Сделать анализ по общим пользователям'
     SecondFunction = 'Найти вектор комментариев'
-    AddAdmin = 'Добавить админа'
+    AddModerator = 'Добавить модератора'
     ShowUniqueUsers = 'Посмотреть уникальных пользователей'
-    ShowHistory = 'Посмотреть историю обращений'
+    ShowCountStarts = 'Посмотреть историю обращений'
+    ShowHistory = 'Посмотреть историю запросов'
     Back = 'Вернуться'
+    Yes = 'Да ✅'
+    Favorite = 'Избранные группы ⭐'
+    GetActual = 'Актуализировать 🔄'
+    UseExists = 'Использовать имеющиеся 📚'
 
 
 class ErrorsType(Enum):
-    Welcome = open('helpingFiles/Welcome.txt').read()
-    EndingFirstFunc = open('helpingFiles/EndFirstFunc.txt').read()
-    ErrorInputGroup = open('helpingFiles/ErrorFoundGroup.txt').read()
-    ErrorFoundComments = open('helpingFiles/ErrorFoundComments.txt').read()
-    ErrorFoundGroup = open('helpingFiles/ErrorFoundGroup.txt').read()
+    Welcome = open('samplesErrors/Welcome.txt').read()
+    EndingFirstFunc = open('samplesErrors/EndFirstFunc.txt').read()
+    ErrorInputGroup = open('samplesErrors/ErrorFoundGroup.txt').read()
+    ErrorFoundComments = open('samplesErrors/ErrorFoundComments.txt').read()
+    ErrorFoundGroup = open('samplesErrors/ErrorFoundGroup.txt').read()
+
+
+class HistoryText(Enum):
+    start = 'Запуск бота'
+    history = 'Запрос истории запросов'
+    admin_try = 'Попытка авторизации в административной панели'
+    admin_login = 'Успешная авторизация в административной панели'
+    view_unique = 'Просмотр уникальных пользователей'
+    admin_history = 'Просмотр истории запросов пользователя'
+    add_admin = 'Добавление администратора/модератора'
+    help = 'Вызов помощи'
+    favorites = 'Просмотр избранного'
+    comments = 'Анализ комментариев'
+    groups = 'Подсчет общих подписчиков'
