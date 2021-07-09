5a3886fec37c6a961cbf6fb7009dce2ec8b8dda7
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2021-07-10 00:54:31 +0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,23 +2,20 @@ from telebot import types
 
 import enums
 
+
 class Buttons:
-
-    def __init__(self, type_buttons):
-        self.type_buttons = type_buttons
-
-    def createButton(self, name):
-        if self.type_buttons == 'unusual':
-            return
+    @staticmethod
+    def createButton(name):
         if name == enums.ButtonsType.FunctionalPanel:
             keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
             buttonFirst = types.KeyboardButton('Сделать анализ по общим пользователям')
             buttonSecond = types.KeyboardButton('Найти вектор комментариев')
-            keyboard.add(buttonFirst, buttonSecond)
+            buttonThird = types.KeyboardButton('Избранные группы ⭐')
+            keyboard.add(buttonFirst, buttonThird, buttonSecond)
             return keyboard
         elif name == enums.ButtonsType.AdminPanel:
             keyboard = types.ReplyKeyboardMarkup(row_width=4, resize_keyboard=True)
-            buttonFirst = types.KeyboardButton('Добавить админа')
+            buttonFirst = types.KeyboardButton('Добавить модератора')
             buttonSecond = types.KeyboardButton('Посмотреть уникальных пользователей')
             buttonThird = types.KeyboardButton('Посмотреть историю обращений')
             buttonFourth = types.KeyboardButton('Вернуться')
@@ -37,3 +34,27 @@ class Buttons:
             return keyboard
         elif name == enums.ButtonsType.RemoveButtons:
             return types.ReplyKeyboardRemove()
+        elif name == enums.ButtonsType.ModeratorPanel:
+            keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+            buttonFirst = types.KeyboardButton('Посмотреть уникальных пользователей')
+            buttonSecond = types.KeyboardButton('Посмотреть историю обращений')
+            buttonThird = types.KeyboardButton('Вернуться')
+            keyboard.add(buttonFirst, buttonSecond, buttonThird)
+            return keyboard
+        elif name == enums.ButtonsType.YesOrNo:
+            keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+            buttonFirst = types.KeyboardButton('Да ✅')
+            buttonSecond = types.KeyboardButton('Нет ❌')
+            keyboard.add(buttonFirst, buttonSecond)
+            return keyboard
+        elif name == enums.ButtonsType.ActualButtons:
+            keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+            buttonFirst = types.KeyboardButton(enums.ButtonsType.GetActual.value)
+            buttonSecond = types.KeyboardButton(enums.ButtonsType.UseExists.value)
+            keyboard.add(buttonFirst, buttonSecond)
+            return keyboard
+        elif name == enums.ButtonsType.Stop:
+            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+            button = types.KeyboardButton(enums.ButtonsType.Stop.value)
+            keyboard.add(button)
+            return keyboard
