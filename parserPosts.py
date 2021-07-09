eeb6328018104cc089f343d479545cf85b3f5a0e
Ryouidenshi <55030527+Ryouidenshi@users.noreply.github.com>
2021-07-10 00:56:24 +0500

    Update parserPosts.py

--- parserPosts.py
+++ parserPosts.py
@@ -6,10 +6,11 @@ import requests
 class ParserPosts:
     token = '97e0797797e0797797e07977089798170c997e097e07977f73dfa498a09c0bebff270ba'
 
-    def __init__(self, groupDomain):
+    def __init__(self, groupDomain, last=False):
         self.groupDomain = groupDomain
+        self.last = last
 
-    def get_response(self, offsetCount):
+    def getResponse(self, offsetCount):
         return requests.get('https://api.vk.com/method/wall.get',
                             params={
                                 'access_token': self.token,
@@ -20,25 +21,43 @@ class ParserPosts:
                                 'offset': offsetCount
                             }).json()['response']
 
-    def get_idPosts(self):
-        listIdPosts = []
-        response = self.get_response(0)
+    def getPosts(self):
+        posts = {}
+        response = self.getResponse(0)
         maxOffset = response['count']
         offset = 0
         threads = list()
         while True:
-            if offset >= maxOffset or offset >= 1000:
+            if offset >= maxOffset or offset >= 10000:
                 break
-            x = threading.Thread(target=self.add_posts, args=(listIdPosts, offset))
+            x = threading.Thread(target=self.addPosts, args=(posts, offset))
             offset += 100
             threads.append(x)
             x.start()
-        [thread.join() for thread in threads]
-        return listIdPosts
 
-    def add_posts(self, listIdPosts, offsetCount):
-        for i in self.get_response(offsetCount)['items']:
-            listIdPosts.append(i['id'])
+            if self.last is not None and self.last in posts:
+                break
+
+        [thread.join() for thread in threads]
+        return posts
+
+    def getPostsSolo(self):
+        posts = {}
+        response = self.getResponse(0)
+        maxOffset = response['count']
+        offset = 0
+        while True:
+            if offset >= maxOffset or offset >= 10000 or self.last is not None and self.last in posts:
+                break
+
+            self.addPosts(posts, offset)
+            offset += 100
+
+        return posts
+
+    def addPosts(self, listIdPosts, offsetCount):
+        for i in self.getResponse(offsetCount)['items']:
+            listIdPosts[i['id']] = i['text']
 
     def __del__(self):
         print('Deleted')
